6377b4c56dbab8b79979367f6d0d913b1927be7b
Arthur Gouveia <arthur.gouveia@gmail.com>
2015-10-06 16:59:29 -0300

    Adds config.rb file to allow easy Compass Watch running and fixes issue with photo grid

---
+++ config.rb
@@ -0,0 +1,14 @@
+require 'compass/import-once/activate'
+
+http_path = "/"
+css_dir = "assets/css"
+sass_dir = "assets/sass"
+images_dir = "assets/images"
+javascripts_dir = "assets/js"
+output_style = :compressed
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
